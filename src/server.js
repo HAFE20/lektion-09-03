58e98fdba2a01cd11cb4d99d3b9d3a314afebc84
David Andersson <david.andersson@zocom.se>
2021-09-03 14:42:34 +0200

    Static middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // Importera Express + middleware
 const express = require('express')
+const cors = require('cors')
+
 
 // Importera datan (databas)
 const tools = require('./data.js')
@@ -7,12 +9,29 @@ const tools = require('./data.js')
 // Konfigurera servern
 const app = express()
 const PORT = 1337
-
+let countRequests = 0
 
 // Installera middleware på servern
+app.use( cors() )  // behövs egentligen bara för publika API:n
+
 // app.use( express.urlencoded )  <- används vid autentisering
 app.use( express.json() )  // hantera JSON body vid POST request
 
+app.use( (req, res, next) => {
+	// Logger - skriver ut information som vi kan använda för att debugga
+	// Vi vill veta: vilken HTTP method, vilken resurs, kanske mera?
+	countRequests++
+	console.log(`${countRequests}:  ${req.method}  ${req.url}`, req.body);
+
+	// Alla middleware måste sluta antingen med res.send eller next()
+	next()
+} )
+
+app.use( '/web', express.static(__dirname + '../public') )
+
+
+// JSON.parse()       sträng -> objekt
+// JSON.stringify()   objekt -> sträng
 
 
 
@@ -51,7 +70,7 @@ app.get('/tools/:index', (req, res) => {
 })
 
 app.post('/tools', (req, res) => {
-	console.log('POST /tools, body=', req.body);
+	// console.log('POST /tools, body=', req.body);
 	// Kontrollera att req.body är korrekt utformat
 	let maybeBody = req.body
 	if( !isToolsObject(maybeBody) ) {
